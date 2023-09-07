3ef914248565227dd3b7217c488790f80e30955b
nemcov <dimas224@yandex.ru>
2023-09-07 14:20:49 +0300

    Добавлен тест метода получения списка кофе

---
+++ src/test/java/ru/nemcov/coffeeMaker/unit/CoffeeServiceTest.java
@@ -0,0 +1,18 @@
+package ru.nemcov.coffeeMaker.unit;
+
+import org.junit.jupiter.api.Test;
+import ru.nemcov.coffee_maker.repo.CoffeeRepo;
+import ru.nemcov.coffee_maker.service.CoffeeService;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+class CoffeeServiceTest {
+
+    private final CoffeeRepo coffeeRepo = mock(CoffeeRepo.class);
+    private final CoffeeService service = new CoffeeService(coffeeRepo);
+
+    @Test
+    void shouldGetAllCoffee() {
+        assertThat(service.getAll()).isNotNull();
+    }
+}
